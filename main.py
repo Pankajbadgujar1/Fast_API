54624c5014351afb4306220e60f9e7f4bc29d0e4
Pankaj kumar <pankaj14badgujar@gmail.com>
2026-01-05 12:57:28 +0530

    create first endpoint

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
-
+import json
 app = FastAPI()
+
+def load_data():
+    with open("patients.json", "r") as file:
+        data = json.load(file)
+    return data
+
 @app.get("/")
 def hello():
     return {"message": "Hello, World!"}
 
 @app.get("/about")
 def about():
-    return {"message": "This is the about page."}  
+    return {"message": "A fully functional API  to manage patient records."}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data 
