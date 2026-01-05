02d82d84ca77c1ca49383d5379eba91e142135f0
Pankaj kumar <pankaj14badgujar@gmail.com>
2026-01-05 13:30:39 +0530

    use path() function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 import json
+
 app = FastAPI()
 
 def load_data():
@@ -21,3 +22,12 @@ def view():
     return data 
 
 # Learn Path parameter and Query parameter
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str = Path(..., description='Id of the patient in the Database', example='P001') ):
+    
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        return {"error": "Patient not found"}
